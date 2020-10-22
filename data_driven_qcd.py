71765b92a52476a7a20913d3c6fdac10562085ef
Alp Akpinar <aakpinar@bu.edu>
2020-10-22 12:24:00 -0400

    Update input

--- data_driven_qcd.py
+++ data_driven_qcd.py
@@ -570,7 +570,7 @@ def tf_prediction(outdir,region):
 
 def main():
     # Input handling
-    indir = "./input/merged_2020-10-21_vbfhinv_03Sep20v7_qcd_estimation"
+    indir = "./input/merged_2020-10-22_vbfhinv_03Sep20v7_qcd_estimation"
     acc = klepto_load(indir)
     acc.load('sumw')
     acc.load('sumw_pileup')
