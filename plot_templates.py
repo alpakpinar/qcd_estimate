63cbdf80da540d560e42d5aecf03f37815ccc09b
Alp Akpinar <aakpinar@bu.edu>
2020-10-24 12:27:27 -0400

    Add code to calculate and plot QCD estimate in CR

--- plot_templates.py
+++ plot_templates.py
@@ -7,11 +7,12 @@ import uproot
 import mplhep as hep
 import numpy as np
 from matplotlib import pyplot as plt
+from data_driven_qcd import histdiff
 
 pjoin = os.path.join
 
 def plot_qcd(inpath, fit='nominal', binning='nom', cr_only=False):
-    '''Plot QCD templates in SR and QCD CR.'''
+    '''Plot QCD MC templates in SR and QCD CR.'''
     fpath = pjoin(inpath, f'templates_sr_vbf_qcd_{fit}_bin_{binning}.root')
 
     # Output directory to save plots
@@ -45,6 +46,47 @@ def plot_qcd(inpath, fit='nominal', binning='nom', cr_only=False):
             outname = f'qcd_templates_bin_{binning}_{year}.pdf'
         outpath = pjoin(outdir, outname)
         fig.savefig(outpath)
+        plt.close(fig)
+        print(f'MSG% File saved: {outpath}')
+
+def plot_data_driven_qcd_in_cr(inpath, fit='nominal', binning='nom'):
+    '''In the QCD CR, plot the data driven QCD estimation, e.g. Data - Non-QCD bkg'''
+    fpath = pjoin(inpath, f'templates_sr_vbf_qcd_{fit}_bin_{binning}.root')
+
+    # Output directory to save plots
+    outdir = pjoin(inpath, 'templates')
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    infile = uproot.open(fpath)
+    for year in [2017, 2018]:
+        # Read the data and non-QCD histograms from file
+        h_cr_data = infile[f'sr_vbf_qcd_{year}_cr_data']
+        h_cr_nonqcd = infile[f'sr_vbf_qcd_{year}_cr_nonqcd']
+
+        # Take the difference of the two to get the data-driven QCD estimate in CR
+        qcd_est_sumw, qcd_est_sumw2 = histdiff(h_cr_data, h_cr_nonqcd)
+
+        # Plot these
+        fig, ax = plt.subplots()
+        hep.histplot(h_cr_data.values, h_cr_data.edges, yerr=np.sqrt(h_cr_data.variances), label='Data')
+        hep.histplot(h_cr_nonqcd.values, h_cr_nonqcd.edges, yerr=np.sqrt(h_cr_nonqcd.variances), label='Non-QCD bkg')
+
+        # Plot the difference 
+        hep.histplot(qcd_est_sumw, h_cr_data.edges, yerr=np.sqrt(qcd_est_sumw2), histtype='errorbar', color='k', label='QCD Estimate in CR')
+
+        ax.set_xlabel(r'$M_{jj} \ (GeV)$')
+        ax.set_ylabel('Counts')
+        ax.legend()
+        ax.set_title(f'QCD Prediction in CR: {year}')
+
+        ax.set_yscale('log')
+        ax.set_ylim(1e-2, 1e8)
+
+        # Save figure
+        outpath = pjoin(outdir, f'qcd_in_cr_{year}.pdf')
+        fig.savefig(outpath)
+        plt.close(fig)
         print(f'MSG% File saved: {outpath}')
 
 def main():
@@ -55,6 +97,8 @@ def main():
         try:
             plot_qcd(inpath, fit='nominal', binning=binning)
             plot_qcd(inpath, fit='nominal', binning=binning, cr_only=True)
+
+            plot_data_driven_qcd_in_cr(inpath, fit='nominal', binning=binning)
         except KeyError:
             print(f'Could not find binning: {binning}, skipping')
             continue
