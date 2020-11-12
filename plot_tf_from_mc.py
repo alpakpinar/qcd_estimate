362cdcc3189204c8f92856cf43879d327c2d895c
Alp Akpinar <aakpinar@bu.edu>
2020-11-11 21:17:22 -0500

    Plot TF for HT>200 as well

--- plot_tf_from_mc.py
+++ plot_tf_from_mc.py
@@ -12,21 +12,32 @@ from pprint import pprint
 
 pjoin = os.path.join
 
-def plot_tf_from_mc(acc, outdir, variable='mjj'):
+data_err_opts = {
+    'linestyle':'none',
+    'marker': '.',
+    'markersize': 10.,
+    'color':'k'
+}
+
+def plot_tf_from_mc(acc, outdir, variable='mjj', ht_startsfrom_200=False):
     '''Calculate and plot the TF from QCD MC.'''
     acc.load(variable)
     h = acc[variable]
 
     h = merge_extensions(h, acc, reweight_pu=False)
     scale_xs_lumi(h)
-    h = merge_datasets(h)
+    if not ht_startsfrom_200:
+        h = merge_datasets(h)
 
     if variable == 'mjj':
         mjj_ax = hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200,400,600,900,1200,1500,2000,2750,3500,5000])
         h = h.rebin('mjj', mjj_ax)
 
     for year in [2017, 2018]:
-        _h = h.integrate('dataset', f'QCD_HT_{year}')
+        if not ht_startsfrom_200:
+            _h = h.integrate('dataset', f'QCD_HT_{year}')
+        else:
+            _h = h[re.compile(f'QCD_HT(?!(50to|100to)).*{year}')].integrate('dataset')
         hnum = _h.integrate('region', 'sr_vbf_qcd_regionB')
         hden = _h.integrate('region', 'sr_vbf_qcd_regionA')
 
@@ -40,13 +51,18 @@ def plot_tf_from_mc(acc, outdir, variable='mjj'):
         ax.set_yscale('log')
         ax.set_ylim(1e-3,1e6)
 
-        hist.plotratio(hnum, hden, ax=rax, unc='num')
+        hist.plotratio(hnum, hden, ax=rax, unc='num', error_opts=data_err_opts)
 
         rax.grid(True)
-        rax.set_ylim(0,2)
+        rax.set_ylim(0,1)
         rax.set_ylabel('MC based TF')
 
-        outpath = pjoin(outdir, f'qcd_mc_regionab_{year}.pdf')
+        if ht_startsfrom_200:
+            outname = f'qcd_mc_regionab_{year}_ht200.pdf'
+        else:
+            outname = f'qcd_mc_regionab_{year}.pdf'
+
+        outpath = pjoin(outdir, outname)
         fig.savefig(outpath)
         plt.close(fig)
         print(f'File saved: {outpath}')
@@ -63,7 +79,8 @@ def main():
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
-    plot_tf_from_mc(acc, outdir)
+    plot_tf_from_mc(acc, outdir, ht_startsfrom_200=False)
+    plot_tf_from_mc(acc, outdir, ht_startsfrom_200=True)
 
 if __name__ == '__main__':
     main()
